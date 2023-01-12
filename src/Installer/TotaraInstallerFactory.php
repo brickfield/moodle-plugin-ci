eadfbc87614ec8dbebae236ca4bfc32a4287d6b4
Mike Churchward <mike@brickfieldlabs.ie>
2023-01-12 14:50:31 -0500

    TW20176364 More Totara classes.

--- src/Installer/TotaraInstallerFactory.php
+++ src/Installer/TotaraInstallerFactory.php
@@ -13,7 +13,7 @@
 namespace MoodlePluginCI\Installer;
 
 use MoodlePluginCI\Bridge\Moodle;
-use MoodlePluginCI\Bridge\MoodleConfig;
+use MoodlePluginCI\Bridge\TotaraConfig;
 use MoodlePluginCI\Bridge\MoodlePlugin;
 use MoodlePluginCI\Installer\Database\AbstractDatabase;
 use MoodlePluginCI\Process\Execute;
@@ -85,7 +85,7 @@ class TotaraInstallerFactory
      */
     public function addInstallers(InstallerCollection $installers)
     {
-        $installers->add(new MoodleInstaller($this->execute, $this->database, $this->moodle, new MoodleConfig(), $this->repo, $this->branch, $this->dataDir));
+        $installers->add(new MoodleInstaller($this->execute, $this->database, $this->moodle, new TotaraConfig(), $this->repo, $this->branch, $this->dataDir));
         $installers->add(new PluginInstaller($this->moodle, $this->plugin, $this->pluginsDir, $this->dumper));
         $installers->add(new VendorInstaller($this->moodle, $this->plugin, $this->execute, $this->nodeVer));
 
