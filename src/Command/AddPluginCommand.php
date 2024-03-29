f31329921a0d484c703098268bac0880b5904515
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2024-03-30 00:05:41 +0100

    Bump various dependencies
    - moodle-cs 3.3.4
    - moodle-local_ci 1.0.29
    - moodle-local_moodlecheck 1.3.2
    
    Also some other dependencies and tools updates.

--- src/Command/AddPluginCommand.php
+++ src/Command/AddPluginCommand.php
@@ -80,9 +80,9 @@ class AddPluginCommand extends Command
         $branchCmd = [];
         if (null !== $branch) {
             $branchCmd = [
-                    '--branch',
-                    $branch,
-                ];
+                '--branch',
+                $branch,
+            ];
         }
 
         $cloneCmd = array_merge(
